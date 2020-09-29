f31e2cd1d116881dbcda974320527c0c8067c4b3
CBanks <banksc7669@yahoo.com>
2020-09-29 18:02:14 -0400

    fixed bug in js due to typo errors

--- script.js
+++ script.js
@@ -1,15 +1,60 @@
-var startButton = document.getElementById("start-btn")
+const startButton = document.getElementById("start-btn")
+const nextButton = document.getElementById("next-btn")
+const questionContainerElement = document.getElementById("question-container")
+const questionElement = document.getElementById("question")
+const answerButtonsElement = document.getElementById("answer-buttons")
 
-startButton.addEventListener("click", startButton)
+let shuffledQuestions, currentQuestionIndex
 
-function startQuiz() {
+startButton.addEventListener("click", startGame)
+
+function startGame() {
+    startButton.classList.add("hide")
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
+    questionContainerElement.classList.remove("hide")
+    setNextQuestion()
 }
 
 function setNextQuestion() {
+    resetState()
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+function showQuestion(question) {
+    questionElement.innerText = question.question
+    question.answers.forEach(answer => {
+        const button = document.createElement("button")
+        button.innerText = answer.text
+        button.classList.add("btn")
+
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener("click", selectAnswer)
+        answerButtonsElement.appendChild(button)
+    })
+}
+
+function resetState() {
+    nextButton.classList.add("hide")
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild 
+        (answerButtonsElement.firstChild)
+    }
+}
+
+function selectAnswer(e) {
     
 }
 
-function selectAnswer() {
-    
-}
+const questions = [
+    {
+        question: "Commonly used data types DO NOT include:",
+        answer: [
+            { text: "alert", correct: true },
+            { text: "booleans-strings-numbers", correct: false }
+        ]
+    }
+]
 console.log("let the games begin")
